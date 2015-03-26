d7e490d0ca5f2dac0b7a932a891869d352e27c96
a-out <richbone227@gmail.com>
2015-03-26 11:51:03 -0400

    added Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    written_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
