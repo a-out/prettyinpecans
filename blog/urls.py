430f87dd352b787ce0ce2d85a7e5b2acc512f18d
a-out <richbone227@gmail.com>
2015-04-03 12:41:36 -0400

    use slug to identify posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<slug>\S+)/$', views.DetailView.as_view(), name='detail'),
 )
