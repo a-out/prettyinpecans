c69fc696cbc19720784bf7dd03f35b5720b92350
a-out <richbone227@gmail.com>
2015-04-03 12:41:21 -0400

    root url should resolve to index

--- sparklesandspice/urls.py
+++ sparklesandspice/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', include('blog.urls')),
     url(r'^posts/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
