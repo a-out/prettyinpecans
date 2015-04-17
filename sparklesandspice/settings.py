6d5a0f8db58bee73be9b008c5954cca7b9c9b296
a-out <richbone227@gmail.com>
2015-04-16 22:22:51 -0400

    styling admin page with wider margins
       - and a better typeface

--- sparklesandspice/settings.py
+++ sparklesandspice/settings.py
@@ -36,6 +36,25 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': (
+                "django.contrib.auth.context_processors.auth",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
+                "django.contrib.messages.context_processors.messages"
+            )
+        },
+    },
+]
+
 ROOT_URLCONF = 'sparklesandspice.urls'
 
 WSGI_APPLICATION = 'sparklesandspice.wsgi.application'
