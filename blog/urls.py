924b35aafce38ce9031e2761a3ad7a04d8f04de7
a-out <richbone227@gmail.com>
2015-04-29 10:58:48 -0400

    simplified URL scheme

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,9 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'food/$', views.FoodIndexView.as_view(),  name='food_index'),
-    url(r'fashion/$', views.FashionIndexView.as_view(),  name='fashion_index'),
-    url(r'browser/$', views.recipe_browser,  name='recipe_browser'),
-    url(r'^(?P<slug>\S+)/$', views.DetailView.as_view(), name='detail')
+    url(r'posts/food/$', views.FoodIndexView.as_view(),  name='food_index'),
+    url(r'posts/fashion/$', views.FashionIndexView.as_view(),  name='fashion_index'),
+    url(r'posts/travel/$', views.TravelIndexView.as_view(), name='travel_index'),
+    url(r'recipebrowser/$', views.recipe_browser,  name='recipe_browser'),
+    url(r'^posts/(?P<slug>\S+)/$', views.DetailView.as_view(), name='detail')
 )
--- sparklesandspice/urls.py
+++ sparklesandspice/urls.py
@@ -3,11 +3,9 @@ from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('blog.urls')),
-    url(r'^posts/', include('blog.urls')),
-    url(r'^recipes/', include('blog.urls')),
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  {
+     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  {
         'document_root': settings.MEDIA_ROOT,
     }),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls'))   
 )
